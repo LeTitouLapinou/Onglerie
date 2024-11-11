8814262800d16c22c3bf01afb2f62f895ad5e4df
LeTitouLapinou <162844593+LeTitouLapinou@users.noreply.github.com>
2024-11-11 17:05:43 -0500

    Random nail from prefab lsit

--- Assets/Hand.cs
+++ Assets/Hand.cs
@@ -1,13 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
 public class Hand : MonoBehaviour
 {
 
-    public GameObject nail;
+    public GameObject nail_collection;
 
     public int nail_number = 5;
+
     public Color nail_color;
 
     public Transform[] locators;
@@ -17,8 +19,16 @@ public class Hand : MonoBehaviour
     {
         nail_color = UnityEngine.Random.ColorHSV(0f, 1f, 1f, 1f, 0.5f, 1f);
 
+        for(int i = 0; i < nail_collection.transform.hierarchyCount; i++)
+        {
+            //ESSAYER d'ajouter tous les child du prefab dans une liste, pour ensuite prendre des child random dans la boucle for suivante
+        }
+
+
+
         for (int i = 0; i < locators.Length; i++)
         {
+            GameObject nail = nail_collection.GetComponent<GameObject>();
             GameObject nailInstance = Instantiate(nail, locators[i].position, locators[i].rotation);
             nailInstance.layer = gameObject.layer +1;
             SpriteRenderer rend = nailInstance.GetComponent<SpriteRenderer>();
