9876d47b325865a5ee48a8cc91808bea12104bce
LeTitouLapinou <162844593+LeTitouLapinou@users.noreply.github.com>
2024-11-12 00:04:33 -0500

    Newer nail list prefab

--- Assets/Hand.cs
+++ Assets/Hand.cs
@@ -6,34 +6,27 @@ using UnityEngine;
 public class Hand : MonoBehaviour
 {
 
-    public GameObject nail_collection;
+    public GameObject nail_collectionPrefab;
 
-    public int nail_number = 5;
-
-    public Color nail_color;
+    //public int nail_number = 5;
+    //public Color nail_color;
 
     public Transform[] locators;
+
+    private GameObject[] nailList;
+    private GameObject nail_collectionInstance;
 
     // Start is called before the first frame update
     void Start()
     {
-        nail_color = UnityEngine.Random.ColorHSV(0f, 1f, 1f, 1f, 0.5f, 1f);
+        nail_collectionInstance = Instantiate(nail_collectionPrefab, new Vector3(1000f, 1000f, 1000f), Quaternion.identity);
 
-        for(int i = 0; i < nail_collection.transform.hierarchyCount; i++)
-        {
-            //ESSAYER d'ajouter tous les child du prefab dans une liste, pour ensuite prendre des child random dans la boucle for suivante
-        }
+        //nail_color = UnityEngine.Random.ColorHSV(0f, 1f, 1f, 1f, 0.5f, 1f);
+
+        getChildInNailCollection();
+        InstantiateNailsAtLocators();
 
 
-
-        for (int i = 0; i < locators.Length; i++)
-        {
-            GameObject nail = nail_collection.GetComponent<GameObject>();
-            GameObject nailInstance = Instantiate(nail, locators[i].position, locators[i].rotation);
-            nailInstance.layer = gameObject.layer +1;
-            SpriteRenderer rend = nailInstance.GetComponent<SpriteRenderer>();
-            //rend.color = nail_color;
-        }
     }
 
     // Update is called once per frame
@@ -41,4 +34,32 @@ public class Hand : MonoBehaviour
     {
         
     }
+
+
+    public void getChildInNailCollection()
+    {
+        nailList = new GameObject[nail_collectionInstance.transform.childCount]; //On cree la liste de gameobjects 'nail' depuis la nail_collection
+
+        for (int i = 0; i <  nail_collectionInstance.transform.childCount; i++)
+        {
+            nailList[i] = nail_collectionInstance.transform.GetChild(i).gameObject;
+        }
+
+        foreach (var nail in nailList)
+        {
+            Debug.Log("Nail: " + nail.name);
+        }
+    }
+
+    public void InstantiateNailsAtLocators()
+    {
+        for (int i = 0;i < locators.Length; i++)
+        {
+            GameObject nailInstance = Instantiate(nailList[Random.Range(0,nailList.Length)], locators[i].position, locators[i].rotation); //On choisit un nail au hasard dans la nailList, et on en spawn un a chaque locator
+        }
+    }
+
+
+    //GameObject nailInstance = Instantiate(nail, locators[i].position, locators[i].rotation);
+
 }
